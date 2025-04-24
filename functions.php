3ae54173ba2c094e23012f801a9b226880d58191
smartistacademy <smartistacademy@gmail.com>
2025-04-24 16:17:32 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -34,3 +34,9 @@ function hello_elementor_child_scripts_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+function my_woo_outofstock_text( $text ) {
+	$text = __( 'Verkauft', 'oceanwp' );
+	return $text;
+}
+add_filter( 'ocean_woo_outofstock_text', 'my_woo_outofstock_text', 20 );
